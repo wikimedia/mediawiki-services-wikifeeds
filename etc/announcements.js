d39ef6c11e6cb1254009366ca8c19ca34f24bfd5
cooltey <cfeng@wikimedia.org>
2019-10-21 14:50:58 -0700

    2019 app fundraising announcements
    Bug: T235652,T235537
    Change-Id: I7c261b09ba094346a04d132fe322118402303cbf

--- etc/announcements.js
+++ etc/announcements.js
@@ -27,18 +27,20 @@ const activeWikis = [
 
 const domain = 'en.wikipedia.org';
 
-const startTime = '2018-11-29T16:00:00Z';
-const endTime   = '2018-12-19T23:59:00Z';
+const startTime = '2019-12-02T00:00:00Z';
+const endTime   = '2019-12-31T23:59:00Z';
 const iosMinVersion = '5.8.0';
 const iosMaxVersion = '6.1.0';
-const idPrefix = 'FUNDRAISING18';
+const idPrefix = 'FUNDRAISING19';
 
 const countryVars = {
     us: {
         country: 'the U.S.',
         currency: '$',
         average: 16.36,
-        coffee: 3,
+        coffee: 2.75,
+        border: true,
+        placement: 'article',
         imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/c/cf/Icons-cc-us.png'
     },
     gb: {
@@ -46,27 +48,35 @@ const countryVars = {
         currency: '£',
         average: 10,
         coffee: 2,
+        border: true,
+        placement: 'article',
         imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/6/6f/Icons-cc-gb-ie-au.png'
     },
     au: {
         country: 'Australia',
         currency: '$',
         average: 15,
-        coffee: 3,
+        coffee: 2.75,
+        border: true,
+        placement: 'article',
         imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/6/6f/Icons-cc-gb-ie-au.png'
     },
     ca: {
         country: 'Canada',
         currency: '$',
         average: 15,
-        coffee: 3,
+        coffee: 2.75,
+        border: true,
+        placement: 'article',
         imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/4/4d/Icons-cc-nz-ca.png'
     },
     nz: {
         country: 'New Zealand',
         currency: '$',
         average: 15,
-        coffee: 3,
+        coffee: 2.75,
+        border: true,
+        placement: 'article',
         imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/4/4d/Icons-cc-nz-ca.png'
     },
     ie: {
@@ -74,6 +84,8 @@ const countryVars = {
         currency: '€',
         average: 10,
         coffee: 2,
+        border: true,
+        placement: 'article',
         imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/6/6f/Icons-cc-gb-ie-au.png'
     }
 };
