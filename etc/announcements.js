4af5f119ac3e69d9006ab4abe94ccc6cd1a54ee5
Toni Sevener <tsevener@wikimedia.org>
2020-04-06 18:41:17 -0500

    Add survey announcement for iOS
    iOS needs to lean on the announcements system to display a new survey prompt when they land on an
    article. We need additional info just for this announcement (articleTitles and displayDelay). To
    handle this cleanly, I refactored much of the config variables into campaign objects. With this
    hopefully it will be easier in the future to use this announcements system with multiple campaigns
    running at the same time (for example, a survey and a fundraising campaign on overlapping time
    periods).
    
    Bug: T248507
    Change-Id: If997d1def0f1f6ff04b813bdba1be5f688205302

--- etc/announcements.js
+++ etc/announcements.js
@@ -17,88 +17,93 @@ const AnnouncementType = {
 const Platform = {
     IOS: 'iOSApp',
     IOS_V2: 'iOSAppV2',
+    IOS_V3: 'iOSAppV3',
     ANDROID_V1: 'AndroidApp',
     ANDROID_V2: 'AndroidAppV2'
 };
 
-const activeWikis = [
-    'en.wikipedia.org'
+const campaigns = [
+    {
+        type: AnnouncementType.FUNDRAISING,
+        startTime: '2019-12-02T00:00:00Z',
+        endTime: '2019-12-31T23:59:00Z',
+        idPrefix: 'FUNDRAISING19',
+        domain: 'en.wikipedia.org',
+        activeWikis: [
+            'en.wikipedia.org'
+        ],
+        countryVars: {
+            us: {
+                country: 'the U.S.',
+                currency: '$',
+                average: 16.36,
+                coffee: 2.75,
+                border: true,
+                placement: 'article',
+                imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/c/cf/Icons-cc-us.png'
+            },
+            gb: {
+                country: 'the UK',
+                currency: '£',
+                average: 10,
+                coffee: 2,
+                border: true,
+                placement: 'article',
+                imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/6/6f/Icons-cc-gb-ie-au.png'
+            },
+            au: {
+                country: 'Australia',
+                currency: '$',
+                average: 15,
+                coffee: 2.75,
+                border: true,
+                placement: 'article',
+                imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/6/6f/Icons-cc-gb-ie-au.png'
+            },
+            ca: {
+                country: 'Canada',
+                currency: '$',
+                average: 15,
+                coffee: 2.75,
+                border: true,
+                placement: 'article',
+                imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/4/4d/Icons-cc-nz-ca.png'
+            },
+            nz: {
+                country: 'New Zealand',
+                currency: '$',
+                average: 15,
+                coffee: 2.75,
+                border: true,
+                placement: 'article',
+                imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/4/4d/Icons-cc-nz-ca.png'
+            },
+            ie: {
+                country: 'Ireland',
+                currency: '€',
+                average: 10,
+                coffee: 2,
+                border: true,
+                placement: 'article',
+                imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/6/6f/Icons-cc-gb-ie-au.png'
+            }
+        }
+    },
+    {
+        type: AnnouncementType.SURVEY,
+        startTime: '2020-04-20T00:00:00Z',
+        endTime: '2020-04-27T23:59:00Z',
+        idPrefix: 'SURVEY20',
+        domain: 'en.wikipedia.org',
+        activeWikis: [
+            'en.wikipedia.org'
+        ],
+        countryVars: null
+    }
 ];
 
-const domain = 'en.wikipedia.org';
-
-const startTime = '2019-12-02T00:00:00Z';
-const endTime   = '2019-12-31T23:59:00Z';
-const iosMinVersion = '5.8.0';
-const iosMaxVersion = '6.1.0';
-const idPrefix = 'FUNDRAISING19';
-
-const countryVars = {
-    us: {
-        country: 'the U.S.',
-        currency: '$',
-        average: 16.36,
-        coffee: 2.75,
-        border: true,
-        placement: 'article',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/c/cf/Icons-cc-us.png'
-    },
-    gb: {
-        country: 'the UK',
-        currency: '£',
-        average: 10,
-        coffee: 2,
-        border: true,
-        placement: 'article',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/6/6f/Icons-cc-gb-ie-au.png'
-    },
-    au: {
-        country: 'Australia',
-        currency: '$',
-        average: 15,
-        coffee: 2.75,
-        border: true,
-        placement: 'article',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/6/6f/Icons-cc-gb-ie-au.png'
-    },
-    ca: {
-        country: 'Canada',
-        currency: '$',
-        average: 15,
-        coffee: 2.75,
-        border: true,
-        placement: 'article',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/4/4d/Icons-cc-nz-ca.png'
-    },
-    nz: {
-        country: 'New Zealand',
-        currency: '$',
-        average: 15,
-        coffee: 2.75,
-        border: true,
-        placement: 'article',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/4/4d/Icons-cc-nz-ca.png'
-    },
-    ie: {
-        country: 'Ireland',
-        currency: '€',
-        average: 10,
-        coffee: 2,
-        border: true,
-        placement: 'article',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/donate/6/6f/Icons-cc-gb-ie-au.png'
-    }
-};
-
 module.exports = {
-    activeWikis,
-    domain,
-    countryVars,
-    iosMinVersion,
-    iosMaxVersion,
-    startTime,
-    endTime,
-    idPrefix,
     Platform,
-    AnnouncementType
+    AnnouncementType,
+    campaigns
 };
--- test/lib/announcements.js
+++ test/lib/announcements.js
@@ -8,6 +8,18 @@ const config = require('../../etc/announcements');
 const inactiveAnnouncementDomain = 'cs.wikipedia.org';
 const activeAnnouncementDomain = 'en.wikipedia.org';
 
+const fundraisingCampaigns = config.campaigns.filter((campaign) =>
+    campaign.type == config.AnnouncementType.FUNDRAISING
+);
+
+const surveyCampaigns = config.campaigns.filter((campaign) =>
+    campaign.type == config.AnnouncementType.SURVEY
+);
+
+const fundraisingAndSurveyCampaigns = config.campaigns.filter((campaign) =>
+    campaign.type == config.AnnouncementType.SURVEY || campaign.type == config.AnnouncementType.FUNDRAISING
+);
+
 describe('lib:announcements', () => {
     it('should return no announcement for inactive wiki', () => {
         const res = mut.getAnnouncements(inactiveAnnouncementDomain);
@@ -16,118 +28,179 @@ describe('lib:announcements', () => {
 
     it('should return one or more announcements for active wiki', () => {
         const res = mut.getAnnouncements(activeAnnouncementDomain);
-        if (mut.testing.hasEnded(new Date())) {
+
+        const fundraisingCampaign = fundraisingCampaigns[0];
+        const surveyCampaign = surveyCampaigns[0];
+        if (mut.testing.hasEnded(fundraisingCampaign, new Date()) && mut.testing.hasEnded(surveyCampaign, new Date())) {
             assert.ok(res.announce.length === 0);
-        } else {
+        } else if (mut.testing.hasEnded(fundraisingCampaign, new Date())) {
+            assert.ok(res.announce.length === 1);
+        } else if (mut.testing.hasEnded(surveyCampaign, new Date())) {
             assert.ok(res.announce.length === 18);
+        } else {
+            assert.ok(res.announce.length === 19);
         }
     });
 });
 
 describe('lib:announcements:etc', () => {
-    const announcements = mut.testing.getActiveAnnouncements(activeAnnouncementDomain);
 
-    it('should return no image_url', () => {
-        announcements.forEach((announcement) => {
-            assert.ok(!announcement.image_url);
+    fundraisingCampaigns.forEach((fundraisingCampaign) => {
+
+        const announcements = mut.testing.getAnnouncementsForCampaign(fundraisingCampaign);
+        it('should return no image_url', () => {
+            announcements.forEach((announcement) => {
+                assert.ok(!announcement.image_url);
+            });
+        });
+
+        it('should return correct type', () => {
+            announcements.forEach((elem) => {
+                assert.ok(elem.type === config.AnnouncementType.FUNDRAISING);
+            });
+        });
+
+        it('countries is an array of strings', () => {
+            announcements.forEach((elem) => {
+                assert.ok(elem.countries.every(value => typeof value === 'string'));
+            });
+        });
+
+        it('should not deliver HTML in certain legacy iOS announcements fields', () => {
+            const doc = domino.createDocument();
+            const iosAnnouncement = mut.testing.getLegacyiOSFundraisingAnnouncements(fundraisingCampaign)[0];
+            // destructure 'id', 'text' and 'action.title' from the iOS announcement
+            const { text, action: { title } } = iosAnnouncement;
+            const fieldsToCheck = { text, title };
+            for (const textOnlyFieldName of Object.keys(fieldsToCheck)) {
+                const textToCheck = fieldsToCheck[textOnlyFieldName];
+                const element = doc.createElement('div');
+                element.innerHTML = textToCheck;
+                // Comparing innerHTML and textContent lengths catches even non-tag html,
+                // such as '&nbsp;';
+                assert.deepEqual(
+                    element.innerHTML.length, element.textContent.length,
+                    `iOS does not support HTML in the "${textOnlyFieldName}" field`
+                );
+            }
+        });
+
+        it('iOS legacy fundraising announcement should have the proper platform ID', () => {
+            const announcements = mut.testing.getLegacyiOSFundraisingAnnouncements(fundraisingCampaign);
+            announcements.forEach((announcement) => {
+                assert.ok(announcement.platforms.includes('iOSApp'));
+                assert.ok(!announcement.platforms.includes('iOSAppV2'));
+                assert.ok(!announcement.platforms.includes('iOSAppV3'));
+            });
+        });
+
+        it('iOS fundraising announcement should have the proper platform ID', () => {
+            const announcements = mut.testing.getiOSFundraisingAnnouncements(fundraisingCampaign);
+            announcements.forEach((announcement) => {
+                assert.ok(!announcement.platforms.includes('iOSApp'));
+                assert.ok(announcement.platforms.includes('iOSAppV2'));
+                assert.ok(!announcement.platforms.includes('iOSAppV3'));
+            });
+        });
+
+        it('should deliver HTML in certain V2 announcements fields', () => {
+            const doc = domino.createDocument();
+            const v2Announcement = mut.testing.getAndroidFundraisingAnnouncements(fundraisingCampaign)[0];
+            const { text } = v2Announcement;
+            const fieldsToCheck = { text };
+            for (const textOnlyFieldName of Object.keys(fieldsToCheck)) {
+                const textToCheck = fieldsToCheck[textOnlyFieldName];
+                const element = doc.createElement('div');
+                element.innerHTML = textToCheck;
+
+                // Looking for <br> tags
+                assert.ok(
+                    element.querySelector('BR'),
+                    // eslint-disable-next-line max-len
+                    `V2 announcements should have some HTML line breaks in the "${textOnlyFieldName}" field`
+                );
+            }
+        });
+
+        it('caption_HTML on iOS should be inside a paragraph', () => {
+            // eslint-disable-next-line camelcase
+            const { caption_HTML } = mut.testing.getLegacyiOSFundraisingAnnouncements(fundraisingCampaign)[0];
+            const doc = domino.createDocument(caption_HTML);
+            assert.deepEqual(doc.body.firstElementChild.tagName, 'P');
+        });
+
+        it('caption_HTML on Android should not be inside a paragraph', () => {
+            // eslint-disable-next-line camelcase
+            const { caption_HTML } = mut.testing.getAndroidFundraisingAnnouncements(fundraisingCampaign)[0];
+            const doc = domino.createDocument(caption_HTML);
+            assert.notDeepEqual(doc.body.firstElementChild.tagName, 'P');
         });
     });
 
-    it('should return correct type', () => {
-        announcements.forEach((elem) => {
-            assert.ok(elem.type === config.AnnouncementType.FUNDRAISING);
+    fundraisingAndSurveyCampaigns.forEach(campaign => {
+        it('buildId should not return lower case characters', () => {
+            const id = mut.testing.buildId(campaign, 'IOS', 'US');
+            assert.deepEqual(id, id.toUpperCase());
         });
     });
 
-    it('countries is an array of strings', () => {
-        announcements.forEach((elem) => {
-            assert.ok(elem.countries.every(value => typeof value === 'string'));
+    surveyCampaigns.forEach(campaign => {
+
+        const announcements = mut.testing.getAnnouncementsForCampaign(campaign);
+
+        it('iOS survey announcement should have at least one normalized string in article titles', () => {
+            announcements.forEach((announcement) => {
+                assert.ok(announcement.articleTitles.length > 0);
+                announcement.articleTitles.forEach((title) => {
+                    assert.ok(typeof title == 'string');
+                    assert.ok(!title.includes('_'))
+                });
+            });
         });
-    });
 
-    it('should not deliver HTML in certain legacy iOS announcements fields', () => {
-        const doc = domino.createDocument();
-        const iosAnnouncement = mut.testing.getLegacyiOSAnnouncements(activeAnnouncementDomain)[0];
-        // destructure 'id', 'text' and 'action.title' from the iOS announcement
-        const { text, action: { title } } = iosAnnouncement;
-        const fieldsToCheck = { text, title };
-        for (const textOnlyFieldName of Object.keys(fieldsToCheck)) {
-            const textToCheck = fieldsToCheck[textOnlyFieldName];
-            const element = doc.createElement('div');
-            element.innerHTML = textToCheck;
-            // Comparing innerHTML and textContent lengths catches even non-tag html,
-            // such as '&nbsp;';
-            assert.deepEqual(
-                element.innerHTML.length, element.textContent.length,
-                `iOS does not support HTML in the "${textOnlyFieldName}" field`
-            );
-        }
-    });
+        it('iOS survey announcement should have a displayDelay number > 0', () => {
+            announcements.forEach((announcement) => {
+                assert.ok(typeof announcement.displayDelay == 'number');
+                assert.ok(announcement.displayDelay > 0);
+            });
+        });
 
-    it('should deliver HTML in certain V2 announcements fields', () => {
-        const doc = domino.createDocument();
-        const v2Announcement = mut.testing.getAndroidAnnouncements(activeAnnouncementDomain)[0];
-        const { text } = v2Announcement;
-        const fieldsToCheck = { text };
-        for (const textOnlyFieldName of Object.keys(fieldsToCheck)) {
-            const textToCheck = fieldsToCheck[textOnlyFieldName];
-            const element = doc.createElement('div');
-            element.innerHTML = textToCheck;
-
-            // Looking for <br> tags
-            assert.ok(
-                element.querySelector('BR'),
-                // eslint-disable-next-line max-len
-                `V2 announcements should have some HTML line breaks in the "${textOnlyFieldName}" field`
-            );
-        }
-    });
-
-    it('caption_HTML on iOS should be inside a paragraph', () => {
-        // eslint-disable-next-line camelcase
-        const { caption_HTML } = mut.testing.getLegacyiOSAnnouncements(activeAnnouncementDomain)[0];
-        const doc = domino.createDocument(caption_HTML);
-        assert.deepEqual(doc.body.firstElementChild.tagName, 'P');
-    });
-
-    it('caption_HTML on Android should not be inside a paragraph', () => {
-        // eslint-disable-next-line camelcase
-        const { caption_HTML } = mut.testing.getAndroidAnnouncements(activeAnnouncementDomain)[0];
-        const doc = domino.createDocument(caption_HTML);
-        assert.notDeepEqual(doc.body.firstElementChild.tagName, 'P');
-    });
-
-    it('buildId should not return lower case characters', () => {
-        const id = mut.testing.buildId('IOS', 'US');
-        assert.deepEqual(id, id.toUpperCase());
+        it('iOS survey announcement should have the proper platform ID', () => {
+            announcements.forEach((announcement) => {
+                assert.ok(!announcement.platforms.includes('iOSApp'));
+                assert.ok(!announcement.platforms.includes('iOSAppV2'));
+                assert.ok(announcement.platforms.includes('iOSAppV3'));
+            });
+        });
     });
 
     describe('.hasEnded', () => {
+        const fundraisingCampaign = fundraisingCampaigns[0];
         let oldEndTime;
 
         beforeEach(() => {
-            oldEndTime = config.endTime;
+            oldEndTime = fundraisingCampaign.endTime;
         });
 
         afterEach(() => {
-            config.endTime = oldEndTime;
+            fundraisingCampaign.endTime = oldEndTime;
         });
 
         it('invalid endTime', () => {
-            config.endTime = 'INVALID';
+            fundraisingCampaign.endTime = 'INVALID';
             assert.throws(() => {
-                mut.testing.hasEnded(Date(Date.UTC(2030, 5, 1)));
+                mut.testing.hasEnded(fundraisingCampaign, Date(Date.UTC(2030, 5, 1)));
             }, /config_error/);
         });
 
         it('endTime has passed', () => {
-            config.endTime = '2017-12-20T23:59:00Z';
-            assert.ok(mut.testing.hasEnded(new Date(Date.UTC(2017, 11, 21))));
+            fundraisingCampaign.endTime = '2017-12-20T23:59:00Z';
+            assert.ok(mut.testing.hasEnded(fundraisingCampaign, new Date(Date.UTC(2017, 11, 21))));
         });
 
         it('endTime has not passed yet', () => {
-            config.endTime = '2017-12-20T23:59:00Z';
-            assert.ok(!mut.testing.hasEnded(new Date(Date.UTC(2017, 11, 20))));
+            fundraisingCampaign.endTime = '2017-12-20T23:59:00Z';
+            assert.ok(!mut.testing.hasEnded(fundraisingCampaign, new Date(Date.UTC(2017, 11, 20))));
         });
     });
 
@@ -160,12 +233,15 @@ describe('lib:announcements:etc', () => {
         }
 
         it('all dates should be valid', () => {
-            const startDate = new Date(config.startTime);
-            const endDate = new Date(config.endTime);
 
-            checkDate(startDate, config.startTime, 'startTime');
-            checkDate(endDate, config.endTime, 'endTime');
-            assert.ok(startDate < endDate, 'endTime should be greater than startTime!');
+            fundraisingAndSurveyCampaigns.forEach(campaign => {
+                const startDate = new Date(campaign.startTime);
+                const endDate = new Date(campaign.endTime);
+
+                checkDate(startDate, campaign.startTime, 'startTime');
+                checkDate(endDate, campaign.endTime, 'endTime');
+                assert.ok(startDate < endDate, 'endTime should be greater than startTime!');
+            });
         });
     });
 });
