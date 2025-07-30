8785b78b5298c605845dbade716f9b0aa987e170
Dmitry Brant <dbrant@wikimedia.org>
2025-07-30 09:39:42 -0400

    Introduce remote configuration endpoint.
    Bug: T400814
    Change-Id: I86c6938278a6303564ec7b5d160a6b35cee6b9d1

---
+++ routes/configuration.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const util = require('../lib/util');
+const configuration = require('../lib/configuration');
+
+/**
+ * The main router object
+ */
+const router = util.router();
+
+/**
+ * GET /configuration
+ * Gets remote configuration parameters for various clients.
+ */
+router.get('/configuration', (req, res) => {
+    const json = configuration.getConfiguration(req.params.domain);
+    const hash = util.hashCode(JSON.stringify(json));
+
+    res.status(200);
+    util.setContentType(res, util.CONTENT_TYPES.configuration);
+    util.setETag(res, hash);
+    res.set('Cache-Control', 's-maxage=300, max-age=60');
+    res.json(json);
+});
+
+module.exports = function () {
+    return {
+        path: '/feed',
+        api_version: 1,
+        router
+    };
+};
