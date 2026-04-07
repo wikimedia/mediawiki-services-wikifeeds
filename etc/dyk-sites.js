a895dc27c0e8dd849b93d590361995dd3b7708a6
Dmitry Brant <dbrant@wikimedia.org>
2026-04-07 15:47:47 -0400

    DYK: improve regex for removing "pictured" parenthetical.
    Change-Id: I809149fcb5b8efd78bffdb31d537b9a1b053613a

--- etc/dyk-sites.js
+++ etc/dyk-sites.js
@@ -27,7 +27,7 @@ class DykSite {
 module.exports = {
     ar: new DykSite('ويكيبيديا:هل تعلم', 'section > ul > li', /\s*\(في الصورة\)/i),
     de: new DykSite('Wikipedia:Hauptseite/Schon_gewusst', 'ul > li', /\s*\(Bild\)/i),
-    en: new DykSite('Template:Did_you_know', 'section > ul > li', /\s*\(pictured\)/i),
+    en: new DykSite('Template:Did_you_know', 'section > ul > li', /\s*\([^)]*picture[^)]*\)/i),
     hi: new DykSite('साँचा:क्या_आप_जानते_हैं', 'div > ul > li', /\s*\(चित्रित\)/i),
     pt: new DykSite('Predefinição:Sabia_que', 'section > p:not(.mw-empty-elt)', /\s*\(imagem\)/i),
     ru: new DykSite('Шаблон:Знаете_ли_вы', 'section > ul > li', /\s*\(на\s*илл.\)/i),
