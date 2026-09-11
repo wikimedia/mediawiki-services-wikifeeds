dcc0cc731295cc84f2a95d37afba14299910b64c
Mszwarc <mszwarc@wikimedia.org>
2026-09-11 13:40:58 +0000

    Fix 'news' feed for plwiki
    Adjust the selector, so that it fetches only the actual news content,
    without the instructions on how to maintain the template.
    
    Also, update the test fixture, so that it resembles the current
    content of the plwiki news template.
    
    Bug: T437704
    Change-Id: Ie62f2be91bdd20a383bfac3a84b5b76e924eb115

--- etc/news-sites.js
+++ etc/news-sites.js
@@ -62,7 +62,7 @@ module.exports = {
     he: new NewsSite('תבנית:חדשות_ואקטואליה', 'section > ul > li', TOPIC_SELECTOR_LINK),
     ko: new NewsSite('틀:새로_들어온_소식', 'body > section > ul > li', TOPIC_SELECTOR_BOLD_LINK),
     no: new NewsSite('Mal:Aktuelt', 'ul > li', TOPIC_SELECTOR_BOLD_LINK),
-    pl: new NewsSite('Szablon:Aktualności', 'ul:last-of-type > li', TOPIC_SELECTOR_BOLD_LINK),
+    pl: new NewsSite('Szablon:Aktualności', '.wikifeeds-feed-news ul > li', TOPIC_SELECTOR_BOLD_LINK),
     pt: new NewsSite('Portal:Eventos_atuais', 'table:nth-of-type(2) div:not(.hlist) > ul > li', TOPIC_SELECTOR_BOLD_LINK),
     ru: new NewsSite('Шаблон:Текущие_события_на_заглавной_странице', 'body > section > ul > li', TOPIC_SELECTOR_BOLD_LINK),
     sco: new NewsSite('Template:In_the_news', 'section > ul > li', TOPIC_SELECTOR_BOLD_LINK), // see en
